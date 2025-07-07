f0b6acb7eececa99f4e3a88ec147626f08b4bda7
Arjun113 <arjunsanghi2005@gmail.com>
2025-07-07 22:23:36 +1000

    Added three basic exceptions: server, cache, invaliduser

---
+++ frontend/lib/logic/error/exceptions.dart
@@ -0,0 +1,25 @@
+// Implementing basic exception categories
+
+// An exception for situations where issues lies with the server
+import 'dart:io';
+
+class ServerException implements Exception {
+  final String message;
+
+  ServerException(this.message);
+}
+
+
+// An exception to handle cache problems
+class CacheException implements Exception {
+  final String message;
+
+  CacheException(this.message);
+}
+
+// An exception to handle invalid users
+class InvalidUserException implements Exception {
+  final String message;
+
+  InvalidUserException(this.message);
+}
