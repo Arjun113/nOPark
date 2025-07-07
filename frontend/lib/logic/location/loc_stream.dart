696d7afb8e8506d89e94d1bcc4aab5f8e3df7796
Arjun113 <arjunsanghi2005@gmail.com>
2025-07-07 22:49:32 +1000

    Create a stream of location for mapping purposes

---
+++ frontend/lib/logic/location/loc_stream.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:geolocator/geolocator.dart';
+
+final minimumDistance = 200;
+final timeLimit = 5000; // It is in miliseconds, so 5s
+
+final LocationSettings settings = LocationSettings(
+  accuracy: LocationAccuracy.high,
+  distanceFilter: minimumDistance,
+  timeLimit: Duration(milliseconds: timeLimit)
+);
+
+StreamSubscription<Position> locationStream = Geolocator.getPositionStream(
+  locationSettings: settings
+).listen((Position pos) {
+  // Fill the return statement
+});
