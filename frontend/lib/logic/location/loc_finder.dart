1ef2136d5aa19f31bd8d316a46afaaec6ae467ee
Arjun113 <arjunsanghi2005@gmail.com>
2025-07-07 22:37:12 +1000

    Write location finder (including error handling and requesting elevated accuracy)

---
+++ frontend/lib/logic/location/loc_finder.dart
@@ -0,0 +1,34 @@
+import 'package:geolocator/geolocator.dart';
+
+Future<Position?> getLocationTemp() async {
+  bool locationEnabled;
+  LocationPermission perms;
+  LocationAccuracyStatus accuracy;
+
+  // If permission not there, ask for it
+  locationEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!locationEnabled) {
+    return Future.error('Location services have been disabled on this device.');
+  }
+
+  perms = await Geolocator.checkPermission();
+  if (perms == LocationPermission.denied) {
+    perms = await Geolocator.requestPermission();
+  }
+  else {
+    accuracy = await Geolocator.getLocationAccuracy();
+    if (accuracy == LocationAccuracyStatus.reduced) {
+      Geolocator.requestTemporaryFullAccuracy(purposeKey: 'We require your precise location to be able to navigate you');
+      accuracy = await Geolocator.getLocationAccuracy();
+      if (accuracy == LocationAccuracyStatus.reduced) {
+        return Future.error('Location services cannot be provided.');
+      }
+    }
+  }
+
+  if (perms == LocationPermission.deniedForever) {
+    return Future.error('Location services have been denied forever on this device.');
+  }
+
+  return await Geolocator.getCurrentPosition();
+}
--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import dynamic_color
+import geolocator_apple
+import package_info_plus
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
