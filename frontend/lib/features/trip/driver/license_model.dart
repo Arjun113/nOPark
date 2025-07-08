6b3e6958c74e0de31844fbee2ac17a8bb28f31e8
Arjun113 <arjunsanghi2005@gmail.com>
2025-07-08 19:29:26 +1000

    Add License model

---
+++ frontend/lib/features/trip/driver/license_model.dart
@@ -0,0 +1,42 @@
+import '../../../constants/licensetype.dart';
+import '../../../constants/licenseConditions.dart';
+
+class License {
+  final String licenseNumber;
+  final LicenseType licenseType;
+  final Set<LicenseCondition> licenseConditions;
+
+  License ({
+    required this.licenseNumber,
+    required this.licenseType,
+    required this.licenseConditions
+  });
+
+  Set<LicenseCondition> getConditions() {
+    return this.licenseConditions;
+  }
+
+  bool checkIfConditionPresent (LicenseCondition condition) {
+    return this.licenseConditions.contains(condition);
+  }
+
+  void addCondition (LicenseCondition condition) {
+    this.licenseConditions.add(condition);
+  }
+
+  factory License.fromJson (Map<String, dynamic> json) {
+    return License (
+      licenseType: json['licenseType'],
+      licenseConditions: json['licenseConditions'],
+      licenseNumber: json['licenseNumber']
+    );
+  }
+
+  Map <String, dynamic> toJson () {
+    return {
+      'licenseType': this.licenseType,
+      'licenseConditions': this.licenseConditions.toString(),
+      'licenseNumber': this.licenseNumber
+    };
+  }
+}
