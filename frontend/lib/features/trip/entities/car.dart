ef1be4ad54c42ee0bd7ec2be982e899e3e1bb0a9
Arjun113 <arjunsanghi2005@gmail.com>
2025-07-08 21:02:20 +1000

    Add ride request scaffold

---
+++ frontend/lib/features/trip/entities/car.dart
@@ -0,0 +1,18 @@
+class Car {
+  String carLicensePlate;
+  final String carMake;
+  final String carModel;
+  String carColour;
+  final String carSeatingCapacity;
+  final String carImage;
+
+  Car ({
+    required this.carLicensePlate,
+    required this.carMake,
+    required this.carModel,
+    required this.carImage,
+    required this.carColour,
+    required this.carSeatingCapacity
+  });
+
+}
---
+++ frontend/lib/features/trip/entities/ride_request.dart
@@ -0,0 +1,22 @@
+import 'rideOffer.dart';
+import 'location.dart';
+import '../driver/driver_model.dart';
+import 'car.dart';
+
+class RideRequest {
+  final Driver driver;
+  Location currentLocation;
+  List<RideOffer> rideOffers;
+  final Car car;
+
+  RideRequest({
+    required this.driver,
+    required this.currentLocation,
+    required this.rideOffers,
+    required this.car
+  });
+
+  void addNewAcceptedOffer (RideOffer offer) {
+    this.rideOffers.add(offer);
+  }
+}
