e6d87134e0f63a50ffa8771bdc7eff936478e1d5
Arjun113 <arjunsanghi2005@gmail.com>
2025-07-14 10:47:31 +1000

    created login datamodel

---
+++ frontend/lib/features/authentications/domain/entities/login_payload.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+class LoginPayload extends Equatable{
+  final String email;
+  final String password;
+
+  LoginPayload ({
+    required this.email,
+    required this.password
+  });
+
+  @override
+  List<Object?> get props => <Object>[this.email];
+}
