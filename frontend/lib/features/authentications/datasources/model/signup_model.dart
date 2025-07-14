27615b24a11fa4b06611cd571c621025da216176
Arjun113 <arjunsanghi2005@gmail.com>
2025-07-14 16:09:13 +1000

    add signup model

---
+++ frontend/lib/features/authentications/datasources/model/signup_model.dart
@@ -0,0 +1,62 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import '../../domain/entities/signup_payload.dart';
+
+class SignupModel extends SignupPayload {
+
+  SignupModel ({
+    required super.name,
+    required super.age,
+    required super.monashEmail,
+    required super.password,
+    required super.phoneNumber,
+    required super.role,
+    super.profilePicture
+  });
+
+  factory SignupModel.fromJson (Map<String, dynamic> json) {
+    return SignupModel(
+        name: json['name'],
+        age: json['age'],
+        monashEmail: json['email'],
+        password: json['password'],
+        phoneNumber: json['phone'],
+        role: json['role'],
+        profilePicture: File(json['profilePicturePath'])
+    );
+  }
+
+  Map<String, dynamic> toJson () {
+    return <String, dynamic> {
+      'name': this.name,
+      'age': this.age.toString(),
+      'password': this.password,
+      'monashEmail': this.monashEmail,
+      'profilePicturePath': this.profilePicture?.path
+    };
+  }
+}
+
+class VerifyOtpModel {
+  final int Otp;
+  final bool signedUp;
+
+  const VerifyOtpModel ({
+    required this.Otp,
+    required this.signedUp
+});
+
+  factory VerifyOtpModel.fromJson (Map<String, dynamic> json) {
+    return VerifyOtpModel(Otp: int.parse(json['otp']), signedUp: bool.parse(json['signedUp']));
+  }
+
+  Map<String, dynamic> toJson () {
+    return <String, dynamic> {
+      'otp': this.Otp.toString(),
+      'signedUp': this.signedUp.toString()
+    };
+  }
+
+
+}
--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,9 +9,11 @@ import connectivity_plus
 import dynamic_color
 import firebase_auth
 import firebase_core
+import flutter_secure_storage_macos
 import geolocator_apple
 import package_info_plus
 import path_provider_foundation
+import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -19,8 +21,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
