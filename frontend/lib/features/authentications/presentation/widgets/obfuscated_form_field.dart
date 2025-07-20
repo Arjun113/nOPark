28e7c0183f04adb2400ab0363cc21107faa2749d
Arjun113 <arjunsanghi2005@gmail.com>
2025-07-20 19:14:55 +1000

    add obfuscated form field widget

---
+++ frontend/lib/features/authentications/presentation/widgets/obfuscated_form_field.dart
@@ -0,0 +1,40 @@
+// Purpose: obsfucated data such as passwords
+
+import 'package:flutter/material.dart';
+
+class ObfuscatedFormField extends StatefulWidget{
+  final String fieldTitle;
+  final TextEditingController controller;
+
+  const ObfuscatedFormField({super.key, required this.fieldTitle, required this.controller});
+
+  @override
+  State<StatefulWidget> createState() {
+    return ObfuscatedFormFieldState();
+  }
+}
+
+class ObfuscatedFormFieldState extends State<ObfuscatedFormField>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+            widget.fieldTitle
+        ),
+        SizedBox(height: 5),
+        Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Theme.of(context).primaryColor
+          ),
+          child: TextField(
+            obscureText: true,
+          ),
+        )
+      ],
+    );
+  }
+}
