9669378ae36e5d7275a4ada90202549b15e92132
Arjun113 <arjunsanghi2005@gmail.com>
2025-07-21 18:25:03 +1000

    Add a big button for roles

---
+++ frontend/lib/features/authentications/presentation/widgets/role_button.dart
@@ -0,0 +1,78 @@
+//Purpose - Assign roles
+
+import 'package:flutter/material.dart';
+
+class RoleButton extends StatefulWidget {
+  final String role;
+  final String description;
+  final Icon icon;
+  final Color color;
+
+  const RoleButton({
+    super.key,
+    required this.role,
+    required this.icon,
+    required this.description,
+    required this.color
+});
+
+  @override
+  State<StatefulWidget> createState() {
+    return RoleButtonState();
+  }
+}
+
+
+class RoleButtonState extends State<RoleButton> {
+  bool selected = false;
+  Color fillCol = Colors.grey.shade400;
+
+  void selectionChange () {
+    if (selected == true) {
+      selected == false;
+      fillCol = Colors.grey.shade400;
+    }
+    else {
+      selected == true;
+      fillCol = Colors.grey.shade600;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: selectionChange,
+        child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 5, color: widget.color),
+          color: fillCol,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(widget.role,
+                style: TextStyle(
+                  fontSize: 50
+                ),),
+                SizedBox(height: 10,),
+                widget.icon
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  widget.description,
+                  style: TextStyle(
+                    fontSize: 25
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
