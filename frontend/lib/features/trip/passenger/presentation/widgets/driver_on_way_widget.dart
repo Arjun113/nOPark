1b40e850f5dd50931745d9f924e4ea6383d5c791
Arjun113 <arjunsanghi2005@gmail.com>
2025-07-29 10:38:52 +1000

    Driver on the way widget added

---
+++ frontend/lib/features/trip/passenger/presentation/widgets/driver_on_way_widget.dart
@@ -0,0 +1,45 @@
+// Purpose: Showcase the driver being 'on the way'
+
+import 'package:flutter/material.dart';
+
+class DriverOnTheWay extends StatelessWidget {
+  final String carImageURL;
+  final String driverName;
+  final String carRegistration;
+  final String carMakeModel;
+  final String carColour;
+
+  const DriverOnTheWay({
+   super.key,
+   required this.driverName,
+   required this.carImageURL,
+   required this.carMakeModel,
+   required this.carColour,
+   required this.carRegistration
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text("Your driver is on the way", style: Theme.of(context).textTheme.headlineMedium,),
+          Center(
+            child: Image(image: NetworkImage(carImageURL)),
+          ),
+          Center(
+            child: Text(this.carMakeModel, style: Theme.of(context).textTheme.headlineSmall,),
+          ),
+          Center(
+            child: Text(this.carColour, style: Theme.of(context).textTheme.headlineSmall,)
+          ),
+          const SizedBox(height: 15,),
+          Center(
+            child: Text(this.carRegistration, style: Theme.of(context).textTheme.titleMedium,),
+          )
+        ],
+      ),
+    );
+  }
+}
