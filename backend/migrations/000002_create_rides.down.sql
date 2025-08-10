64c34ff0efdbdd8dd8ab8ae1ee05498c406aefe9
Lachlan MacPhee <github@lachlanmacphee.com>
2025-08-10 22:49:37 +1000

    feat: add is_sent column to notification and down migration scripts

---
+++ backend/migrations/000002_create_rides.down.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS proposals;
+DROP TABLE IF EXISTS requests;
+DROP TABLE IF EXISTS rides;
--- backend/migrations/000006_create_notifications.up.sql
+++ backend/migrations/000006_create_notifications.up.sql
@@ -5,9 +5,12 @@ CREATE TABLE notifications (
     notification_type VARCHAR(50) NOT NULL CHECK (notification_type IN ('review', 'proximity', 'ride_status')),
     notification_message VARCHAR(100) NOT NULL,
     account_id BIGINT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+    is_sent BOOLEAN DEFAULT FALSE NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 -- Indices -------------------------------------------------------
+
+CREATE INDEX idx_notifications_sent_created ON notifications(is_sent, created_at) WHERE is_sent = FALSE;
 
 -- Triggers ------------------------------------------------------
