a0f534d9f108501ff515fba988944b0185ba3cb4
Arjun113 <arjunsanghi2005@gmail.com>
2025-08-27 16:54:43 +1000

    new branch to resolve firebase issues

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -1,25 +1,12 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
 
-dependencies {
-    // Import the Firebase BoM (Bill of Materials)
-    // The BoM ensures all Firebase libraries you use are compatible versions.
-    implementation(platform("com.google.firebase:firebase-bom:32.0.0")) // Use the latest stable BoM version
-
-    // Add the dependency for Firebase Authentication
-    implementation("com.google.firebase:firebase-auth")
-
-    // Add the dependency for Firebase Cloud Messaging
-    implementation("com.google.firebase:firebase-messaging")
-}
-
 android {
-    namespace = "com.example.nopark"
+    namespace = "com.android.nopark"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -33,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.nopark"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.android.nopark"
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -45,11 +29,15 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:32.0.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-messaging")
 }
 
 flutter {
