2692a52feab71380389e66e2d09ff3de3f88e573
Arjun113 <arjunsanghi2005@gmail.com>
2025-08-28 19:47:25 +1000

    initialized firebase

--- frontend/lib/home_test.dart
+++ frontend/lib/home_test.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:nopark/features/feeds/presentation/screens/test_home.dart';
 
 import 'features/trip/entities/user.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
    runApp(const MyApp());
 }
 
