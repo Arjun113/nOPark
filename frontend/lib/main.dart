94c9b07ec7f20b6d669e34e1f2db3f3c683aa181
Lachlan MacPhee <github@lachlanmacphee.com>
2025-10-08 15:19:37 +1100

    fix: clean main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,41 +1,5 @@
-// import 'package:flutter/material.dart';
-// import 'package:dynamic_color/dynamic_color.dart';
-// import 'package:nopark/logic/utilities/theme_builder.dart';
-
-// void main() {
-//   runApp(const MainApp());
-// }
-
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
-
-//   static final Color fallbackSeed = Colors.blueAccent;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return DynamicColorBuilder(
-//       builder: (dynamicLightTheme, dynamicDarkTheme) {
-//         final dynamicLightScheme =
-//             dynamicLightTheme ?? ColorScheme.fromSeed(seedColor: fallbackSeed);
-//         final dynamicDarkScheme =
-//             dynamicDarkTheme ??
-//             ColorScheme.fromSeed(
-//               seedColor: fallbackSeed,
-//               brightness: Brightness.dark,
-//             );
-
-//         return MaterialApp(
-//           title: 'nOPark',
-//           debugShowCheckedModeBanner: false,
-//           theme: createTheme(dynamicLightScheme),
-//           darkTheme: createTheme(dynamicDarkScheme),
-//           themeMode: ThemeMode.system,
-//         );
-//       },
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
+
 import 'features/signup/interface/widgets/login_screen.dart';
 import 'features/signup/interface/widgets/register_screen.dart';
 
