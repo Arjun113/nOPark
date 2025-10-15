4929506d5663796b79e98d5b342b84f1e1e1344e
Lachlan MacPhee <github@lachlanmacphee.com>
2025-10-15 18:48:51 +1100

    fix: remove tile server from docker compose

--- frontend/lib/constants/api_uris.dart
+++ frontend/lib/constants/api_uris.dart
@@ -1,2 +1,4 @@
-const openStreetMapUri = 'https://nopark-tiles.lachlanmacphee.com/public.osm_nodes/{z}/{x}/{y}.pbf';
+// const openStreetMapUri =
+//     'https://b.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png';
+const openStreetMapUri = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
 const openStreetMapUserAgent = 'com.nopark.loc';
