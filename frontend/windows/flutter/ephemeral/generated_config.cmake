e20811fc22433f39a95fb9b564787a7f4f8ca3d1
Arjun113 <arjunsanghi2005@gmail.com>
2025-10-15 18:06:10 +1100

    Add default geocoding FEATURE

---
+++ frontend/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\ArjunSanghi\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ArjunSanghi\\Desktop\\S2 2025\\FIT3162\\nOPark\\frontend" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\ArjunSanghi\\dev\\flutter"
+  "PROJECT_DIR=C:\\Users\\ArjunSanghi\\Desktop\\S2 2025\\FIT3162\\nOPark\\frontend"
+  "FLUTTER_ROOT=C:\\Users\\ArjunSanghi\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ArjunSanghi\\Desktop\\S2 2025\\FIT3162\\nOPark\\frontend\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ArjunSanghi\\Desktop\\S2 2025\\FIT3162\\nOPark\\frontend"
+  "FLUTTER_TARGET=C:\\Users\\ArjunSanghi\\Desktop\\S2 2025\\FIT3162\\nOPark\\frontend\\lib\\home_test.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzUuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MDVkYjk2ODkwOA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YThiZmRmYzM5NA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjA="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ArjunSanghi\\Desktop\\S2 2025\\FIT3162\\nOPark\\frontend\\.dart_tool\\package_config.json"
+)
